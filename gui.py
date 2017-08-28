3bd9362f7621e503f0360008691958a3ed77d50e
David Yuan <davidyuan@utexas.edu>
2017-08-28 16:17:05 -0500

    Starter GUI code

---
+++ gui.py
@@ -0,0 +1,42 @@
+import tkinter
+import platform
+import os
+
+
+class Frame(tkinter.Frame):
+    def __init__(self, parent):
+        """Define frame components and instance variables."""
+        tkinter.Frame.__init__(self, parent)
+        self.parent = parent
+        self.button = tkinter.Button(text='Go', command=comp)
+        self.initialize_user_interface()
+
+        # Ensures GUI starts at front.
+        if platform.system().startswith('Darwin'):
+            os.system('''/usr/bin/osascript -e 'tell app "Finder" to set frontmost of process "Python" to true' ''')
+        else:
+            parent.lift()
+
+    def initialize_user_interface(self):
+        """Sets up user interface."""
+        self.parent.title("Lathe 2.0 Interface")
+        self.parent.grid_rowconfigure(0, weight=1)
+        self.parent.grid_columnconfigure(0, weight=1)
+        self.parent.config(background="white")
+
+        self.button.pack()
+
+    def complain(self, string):
+        """Prints out problems to user in pop-up window."""
+        top = tkinter.Toplevel(master=self.parent)
+        top.title("Something went wrong ")
+        msg = tkinter.Message(top, text=string, width=1000)
+        msg.pack(side="top", padx=10, pady=10)
+        button = tkinter.Button(top, text="Dismiss", command=top.destroy)
+        button.pack()
+
+
+if __name__ == "__main__":
+    root = tkinter.Tk()
+    mainFrame = Frame(root)
+    root.mainloop()
